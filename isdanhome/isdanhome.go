4282980772829290e68db76eb57f6a5c0cb800ce
Dan The Dan <dan.delellis@gmail.com>
2019-08-21 08:01:21 +0100

    getting there!

--- isdanhome/isdanhome.go
+++ isdanhome/isdanhome.go
@@ -1,24 +1,41 @@
+# do something like this https://play.golang.org/p/8e1zNrs4a1C
+
 package main
 import (
     "fmt"
-    "os/system"
-    "github.com/juju/fslock"
+    "os/exec"
+    "time"
+    "bytes"
 
 )
 
 func main() {
-    var mac := "40:4e:36:bb:c3:10"
+//    mac := "40:4e:36:bb:c3:10"
+    danHome := true
 
 
-    lockFile := fslock.New("/var/lock/isdanhome")
-    lockErr := lockFile.TryLock()
-    if lockErr != nil {
-        fmt.Println("Couldn't get lockfile!  Shit's bad yo!")
-    }
-
-    time.Sleep(10 * time.Second)
-    lockFile.Unlock()
-
+        if danHome {
+            fmt.Println("dan must be home")
+        } else {
+            fmt.Println("dan is not home")
+        }
+        fmt.Println("I sleep")
+        time.Sleep(10*time.Second)
 }
 
+func pingPhone(mac string) (danStatus bool) {
+    cmd := exec.Command("/usr/bin/l2ping", "-c1", "-d0", mac)
+    var errbuf bytes.Buffer
+    cmd.Stderr = &errbuf
+    err := cmd.Run()
+
+    if err != nil {
+        fmt.Println(errbuf.String())
+        danStatus=false
+        return
+    }
+    danStatus = true
+    return
+
+}
 
