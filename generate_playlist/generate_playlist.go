9b4382036582dde66b849f2bf78428dfb2ffc185
Dan The Dan <dan.delellis@gmail.com>
2019-09-15 14:22:37 -0700

    final version

--- generate_playlist/generate_playlist.go
+++ generate_playlist/generate_playlist.go
@@ -3,90 +3,111 @@ package main
 import (
     "fmt"
     taglib "github.com/wtolson/go-taglib"
+    "flag"
     "time"
     "os"
     "path/filepath"
     "strings"
+    "io/ioutil"
+    "encoding/json"
 )
 
 func main() {
-    musicRoot := "/mnt/music/"
+    settingsFile := flag.String("settings", "settings.json", "settings json file to use")
+    flag.Parse()
 
-    var exclusions Exclusions
+    exclusions := parseSettingsJson(*settingsFile)
 
-    exclusions.Artist = []string{"Armik", "Johannes Linstead", "Elvis", "Foreigner"}
-    exclusions.Genre = []string{"Guitar", "Classical"}
-    exclusions.minTime = "2m"
-    exclusions.maxTime = "10m"
+    if exclusions.Playlist == "" {
+        exclusions.Playlist = "playlist.m3u8"
+    }
+
+    fmt.Println(exclusions)
 
 
-    fileList := getFiles(musicRoot, exclusions)
+    fileList := getFiles(exclusions.Root)
 
     var filesToKeep []string
 
     for _,file := range fileList {
         track := scanFile(file)
-        if keepTrack(track, exclusions) {
-            filesToKeep = append(filesToKeep,track.FilePath)
+        if !keepTrack(track, exclusions) {
+            track.FilePath = strings.Join([]string{"#",track.FilePath}, " ")
         }
+       filesToKeep = append(filesToKeep, track.FilePath)
+
+
     }
 
+    writeFile(filesToKeep,exclusions.Playlist)
+    os.Exit(0)
 
 
-    for _,keep := range filesToKeep {
-        fmt.Println(keep)
-    }
 }
 
 func keepTrack(file trackInfo, drop Exclusions) bool {
     var err error
-    minTime, err := time.ParseDuration(drop.minTime)
-    maxTime, err := time.ParseDuration(drop.maxTime)
+    minTime, err := time.ParseDuration(drop.MinTime)
+    maxTime, err := time.ParseDuration(drop.MaxTime)
 
     if err != nil {
         os.Exit(1)
     }
 
-    for _,artist := range drop.Artist {
-        if strings.Contains(file.Artist, artist) { return false }
+    for _,artist := range drop.ExArtists {
+        if strings.Contains(file.Artist, artist) {
+        fmt.Printf("matched artist: %s\n", file.FilePath)
+            return false
+        }
     }
 
-    for _,album := range drop.Album {
-        if strings.Contains(file.Album, album) { return false }
+    for _,album := range drop.ExAlbums {
+        if strings.Contains(file.Album, album) {
+        fmt.Printf("matched album: %s\n", file.FilePath)
+            return false
+        }
     }
 
-    for _,title := range drop.Title {
-        if strings.Contains(file.Title, title) { return false }
+    for _,title := range drop.ExTitles {
+        if strings.Contains(file.Title, title) {
+        fmt.Printf("matched title: %s\n", file.FilePath)
+            return false
+        }
     }
 
-    for _,comment := range drop.Comment {
-        if strings.Contains(file.Comment, comment) { return false }
+    for _,comment := range drop.ExComments {
+        if strings.Contains(file.Comment, comment) {
+        fmt.Printf("matched comment: %s\n", file.FilePath)
+            return false
+        }
     }
 
-    for _,genre := range drop.Genre {
-        if strings.Contains(file.Genre, genre) { return false }
+    for _,genre := range drop.ExGenres {
+        if strings.Contains(file.Genre, genre) {
+        fmt.Printf("matched genre: %s\n", file.FilePath)
+            return false
+        }
     }
 
     if file.Duration < minTime || file.Duration > maxTime {
+        fmt.Printf("bad duration: %s\n", file.FilePath)
         return false
     }
 
-    if file.Bitrate < drop.minBitrate {
+    if file.Bitrate < drop.MinBitrate {
+        fmt.Printf("bad bitrate: %s\n", file.FilePath)
         return false
     }
 
     return true
 }
 
-func getFiles(root string, drop Exclusions) (fileList []string) {
+func getFiles(root string) (fileList []string) {
     err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
         if err != nil || info.IsDir() {
             return err
         }
-//        fileInfo := scanFile(path)
-//        if keepTrack(fileInfo, drop) {
         fileList = append(fileList, path)
-//        }
         return nil
 
     })
@@ -120,6 +141,49 @@ func scanFile(filename string) (songData trackInfo) {
     return
 }
 
+func parseSettingsJson(file string) (settings Exclusions) {
+    jsonFile, err := os.Open(file)
+    if err != nil {
+        fmt.Printf("unable to open json file %s: %v\n", file, err)
+        os.Exit(1)
+    }
+
+    jsonBytes, err := ioutil.ReadAll(jsonFile)
+    if err != nil {
+        fmt.Printf("unable to read json file %s: %v\n", file, err)
+        os.Exit(1)
+    }
+
+    err = json.Unmarshal(jsonBytes, &settings)
+    if err != nil {
+        fmt.Printf("Unable to parse json file: %v\n", err)
+    }
+    return
+}
+
+func writeFile(playlist []string, file string) {
+    str := strings.Join(playlist, "\n")
+    f, err := os.Create(file)
+    if err != nil {
+        fmt.Println(str)
+        fmt.Println("Unable to create file, so you can copy/paste the above")
+        fmt.Println(err)
+        os.Exit(1)
+    }
+
+    _, err = f.WriteString(str)
+    if err != nil {
+        fmt.Println(str)
+        fmt.Println("Unable to print data to file.  You can copy/paste the above")
+        fmt.Println(err)
+        f.Close()
+        os.Exit(1)
+    }
+
+    f.Close()
+
+}
+
 type trackInfo struct {
     FilePath    string
     Artist      string
@@ -144,6 +208,7 @@ type Exclusions struct {
 */
 type Exclusions struct {
 	Root       string   `json:"root"`
+        Playlist   string   `json:"playlist"`
 	ExArtists  []string `json:"ex_Artists"`
 	ExAlbums   []string `json:"ex_Albums"`
 	ExTitles   []string `json:"ex_Titles"`
@@ -151,5 +216,5 @@ type Exclusions struct {
 	ExGenres   []string `json:"ex_Genre"`
 	MinTime    string   `json:"min_time"`
 	MaxTime    string   `json:"max_time"`
-	MinBitRate int      `json:"min_bit_rate"`
+	MinBitrate int      `json:"min_bit_rate"`
 }
