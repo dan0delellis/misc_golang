b8acbc5e97a56e3efa8265dc57eb70ce984a3d6e
Daniel De Lellis <dan.delellis@gmail.com>
2025-02-04 10:38:28 +0000

    spell 'Marquee' correctly. start some logic but don't do anything yet

--- lcd_readout/main.go
+++ lcd_readout/main.go
@@ -6,26 +6,36 @@ import (
 )
 const wait = 3
 func main() {
-    lcd, err := initLCD("/dev/ttyACM0")
+    lcd, err := InitLCD("/dev/ttyACM0")
     if err != nil {
         fmt.Println(err)
         return
     }
-    defer lcd.Close()
-    lcd.Printf("hello!\ndo you like cheese")
-    time.Sleep(wait*time.Second)
     lcd.Clear()
-    lcd.SetBG(0,128,32)
-    lcd.Print("now I am green")
-    time.Sleep(wait*time.Second)
-    lcd.Clear()
-    lcd.SetBG(32,32,128)
-    lcd.Print("good bye")
-    time.Sleep(wait*time.Second)
-    lcd.Clear()
-    lcd.SetBG(0,0,0)
-}
-// Marqee will scroll the given text across one line.
-func (d Display) Marqee(s string) {
+    lcd.BlinkyBlock()
+    lcd.Marquee("NOW WHAT")
 
+    defer lcd.Close()
 }
+
+
+// Wait, that's illegal!
+var fmtWidthStr = fmt.Sprintf("% " + fmt.Sprintf("%d",cols) + "s")
+
+// Marquee will scroll the given text across one line. This requires that the width is set correctly or it will look strange
+// Single line breaks, be them \r, \n, or \rn, are replaced with two spaces
+// It will not move the cursor with the text. It will hopefully block all text input while running.
+func (d Display) Marquee(s string) {
+    s = string(singleLineBreak.ReplaceAll([]byte(s), twospace))
+
+    //This is going to be the row of text we display
+    buffer := fmt.Sprintf(fmtWidthStr,"")
+
+
+    for i:=0; i < len(s); i++ {
+        d.Home()
+        d.Print(buffer)
+        time.Sleep(300 * time.Millisecond)
+    }
+}
+
