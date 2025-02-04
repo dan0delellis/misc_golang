e113273fa271f9c5ac494458cfcc871ce61751fa
Daniel De Lellis <dan.delellis@gmail.com>
2025-02-04 10:37:02 +0000

    make Printf call Print. Make print replace all instance of single line breaks with \r\n because that is universally understood as a single line break, including the display

--- lcd_readout/printing.go
+++ lcd_readout/printing.go
@@ -3,12 +3,20 @@ import (
     "fmt"
 )
 
+
+var winderp = []byte{'\r','\n'}
+var twospace = []byte{' ', ' '}
+
 // Printf writes the content to the display, formatted with the provided string and the args Printf expects
 func (d Display) Printf(s string, a ...any) (n int, err error) {
-    return fmt.Fprintf(d, s, a...)
+    return d.Print(fmt.Sprintf(s,a...))
 }
 
-// Write writess the provided string to the display
+// Write writes the provided string to the display
 func (d Display) Print(s string) (n int, err error) {
-    return fmt.Fprintf(d, s)
+    b := []byte(s)
+    if newlineToCarriage {
+        b = singleLineBreak.ReplaceAll(b, winderp)
+    }
+    return d.Write(b)
 }
