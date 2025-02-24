ca94bac89b22a3a1d088c89115c436437d0e1c59
Daniel De Lellis <dan.delellis@gmail.com>
2025-02-24 00:29:43 -0800

    setting the TargetStat if a conflict is possible. if that field is nil, the program will silently fail when you attempt to call any of the functions in it

--- copy_photos/file_scan.go
+++ copy_photos/file_scan.go
@@ -17,7 +17,7 @@ func getLinkDirs() []string {
 
 //operates on a single dir entry, this function is specific to source files on the mounted SD card
 func findFiles(queue *[]TargetFile, path string, d fs.DirEntry, err error) (error) {
-    debug("working file", path)
+    debug("working file:", path)
     var task TargetFile
 
     if err != nil {
@@ -42,13 +42,13 @@ func findFiles(queue *[]TargetFile, path string, d fs.DirEntry, err error) (erro
             debug(path+" will be copied")
             //continue
         case NeedsVerify:
-            debug(path+" will be copied, if target file is an incomplete file")
+            debug(path+" will be copied, if target file is an incomplete copy")
             //continue
         case Conflict:
             debug("target does not appear to be an incomplete copy of "+path)
             return fmt.Errorf("conflict detected: %s has different contents than %s",task.TargetFile, path)
         default:
-            debug("i have no idea what do")
+            debug("i have no idea what do with:", path, task.TargetFile)
             return fmt.Errorf("unhandled status for copying %s to %s: %d", path, task.TargetFile, task.Action)
     }
 
@@ -64,22 +64,23 @@ func prepareCopy(p string, d fs.DirEntry) (target TargetFile, err error) {
         err = fmt.Errorf("Error generating target paths from source file (%s) info: %v", d.Name(), srcErr)
         return
     }
-    debug("got src file meta", d.Name())
-    if tgt.Info.Size() < minSize {
-        debug("this file is below minsize")
+    debug("got src file meta:", d.Name())
+    if tgt.SourceInfo.Size() < minSize {
+        debug("this file is below minsize", tgt.SourceInfo.Size(), minSize)
         return
     }
 
     //does the file already exist?
     fileInfo, eNoEnt := os.Stat(tgt.TargetFile)
-    if eNoEnt != nil {
-        tgt.Action = NeedsCopy
-    } else {
+    if eNoEnt == nil {
         //no error means a file is already there
-        tgt.Action = compareSrcTgt(tgt.Info, fileInfo)
+        tgt.TargetStat = fileInfo
+        tgt.Action = compareSrcTgt(tgt.SourceInfo, fileInfo)
+    } else {
+        tgt.Action = NeedsCopy
     }
 
-    debug("Destination file ok to (over)write", tgt.TargetFile)
+    debug(fmt.Sprintf("Op level for file %s is %d", tgt.TargetFile, tgt.Action))
 
     target = tgt
     return
@@ -97,19 +98,19 @@ func copyFromDisk(mp string, target TargetFile) (err error) {
     debug("made target/src dir", target.ArchivePath)
 
     //copy from mounted filesystem into archive
-    raw, readErr := readData(mp+"/"+target.SourceFile, target.Info.Size())
+    raw, readErr := readData(mp+"/"+target.SourceFile, target.SourceInfo.Size())
     if readErr != nil {
         err = readErr
         return
     }
-    debug("correct number of bytes read")
+    debug("correct number of bytes read:", target.SourceInfo.Size())
 
     writeErr := writeData(raw, target.ArchiveFile)
     if writeErr != nil {
         err = writeErr
         return
     }
-    debug("wrote file")
+    debug("wrote file ", target.TargetFile)
 
     //hardlink archive to human-friendly
     linkErr := os.Link(target.ArchiveFile, target.SortFile)
@@ -120,7 +121,7 @@ func copyFromDisk(mp string, target TargetFile) (err error) {
     debug("made hardlink", target.SortFile)
 
     //force atime/mtime/ctime to be `mtime.Unix()` in syscall.Utime
-    timeErr := os.Chtimes(target.ArchiveFile, target.Info.ModTime(), target.Info.ModTime())
+    timeErr := os.Chtimes(target.ArchiveFile, target.SourceInfo.ModTime(), target.SourceInfo.ModTime())
     if timeErr != nil {
         err = fmt.Errorf("error setting atime/mtime for copied file (%s) to match source: %v", target.ArchiveFile, timeErr)
         return
@@ -131,17 +132,18 @@ func copyFromDisk(mp string, target TargetFile) (err error) {
 }
 
 func readData(src string, expectedSize int64) (raw []byte, err error) {
+    debug("reading file", src)
     raw, err = os.ReadFile(src)
     if err != nil {
         err = fmt.Errorf("Error trying to read file contents of %s", src, err)
         return
     }
-    debug("read raw")
     readSize := int64(len(raw))
     if readSize != expectedSize {
         err = fmt.Errorf("Wrong number of bytes read (%d) from %s (%d expected)", readSize, src, expectedSize)
         return
     }
+    debug("raw data is of correct expected size:", readSize)
     return
 }
 
@@ -154,7 +156,6 @@ func writeData(data []byte, target string) (err error) {
         if delErr != nil {
             err = fmt.Errorf("%v. Also failed to delete partial file: %v", err, delErr)
         }
-        debug(err)
         return
     }
 
@@ -162,16 +163,19 @@ func writeData(data []byte, target string) (err error) {
 }
 
 func compareSrcTgt(src, tgt fs.FileInfo) int {
-    if tgt.Size() <= src.Size() {
+    if tgt.Size() < src.Size() {
+        debug("target is smaller than source, possible incomplete copy")
         //This may have been an incomplete filecopy
         return NeedsVerify
     }
     if tgt.Size() == src.Size() && tgt.ModTime() == src.ModTime() {
+        debug("target and source have same size/mtime")
         //These are likely the same file
         return NoAction
     }
 
     //some kind of conflict
+    debug("target is larger than source and/or has different mtime")
     return Conflict
 }
 
@@ -180,13 +184,13 @@ func setPermissions(path string, uid, gid int, bitMode fs.FileMode) (error) {
     if chOwnErr != nil {
         return fmt.Errorf("error trying to change owner of copied file (%s) to match dir owner:group (%d:%d)", path, uid, gid)
     }
-    debug("changed owner")
+    debug("changed owner of ", path)
 
     chmodErr := os.Chmod(path, bitMode)
     if chmodErr != nil {
         return fmt.Errorf("Error setting permissions on %s to %v: %v", path, bitMode, chmodErr)
     }
-    debug("set permissions")
+    debug("set permissions on ", path)
 
     return nil
 }
