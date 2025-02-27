e39aa7768abc337db27b97202a8884d7d5ec4ba4
Daniel De Lellis <dan.delellis@gmail.com>
2025-02-27 02:06:39 -0800

    fix issues with metadata strings. make the fslabels var get set to {'fat32', 'exfat'} instead of {'fat32,exfat'}

--- copy_photos/flag_ops.go
+++ copy_photos/flag_ops.go
@@ -9,14 +9,14 @@ type Opts struct {
     TempPrefix  string   `short:"p" long:"tempname" description:"name of directory to create as a temp mountpoint for discovered block device" default:"camera_sd"`
 
     BlkidCache  string   `short:"b" long:"blkidtab" description:"path, relative to CWD, for the contents of blkid" default:"/run/blkid/blkid.tab"`
-    FsTypes     []string `short:"T" long:"fstypes" description:"filesystem type to examine; call multiple times for multiple types" default:"exfat,fat32"`
+    FsTypes     []string `short:"T" long:"fstypes" description:"filesystem type to examine; call multiple times for multiple types" default:"exfat" default:"fat32"`
     FsLabels    []string `short:"l" long:"fslabels" description:"filesystem labels to examine, not case-sensitive" default:"nikon"`
 
-    RootPath    string   `short:"r" long:"rootdir" description:"root of directory path, relative to CWD, to copy files into." default: "photos"`
-    TargetDirs  []string `short:"d" long:"dirname" description: "path, relative to 'rootdir', to create date-formatted directory names. call multiple times to create hardlink clones of the initial directory" default: "."`
+    RootPath    string   `short:"r" long:"rootdir" description:"root of directory path, relative to CWD, to copy files into." default:"photos"`
+    TargetDirs  []string `short:"d" long:"dirname" description:"path, relative to 'rootdir', to create date-formatted directory names. call multiple times to create hardlink clones of the initial directory" default:"."`
     DirFormat   string   `short:"f" long:"dirformat" description:"go-date formatted string for target directory names to move files to" default:"20060102"`
 
-    MinSize     int      `short:"s" long:"minsize" description:"minimum filesize, in bytes, to be included in the file copy list" default: 1000`
+    MinSize     int      `short:"s" long:"minsize" description:"minimum filesize, in bytes, to be included in the file copy list" default:"1000"`
     Verbose     bool     `short:"v" long:"verbose" description:"print ops while processing"`
 }
 
