391913831c427267ad448e4828553228be79f0bb
Daniel De Lellis <dan.delellis@gmail.com>
2025-03-02 12:02:05 -0800

    more flags

--- copy_photos/flag_ops.go
+++ copy_photos/flag_ops.go
@@ -3,18 +3,25 @@ import (
     flags "github.com/jessevdk/go-flags"
 )
 
-//a A B c C D e E F g G H i I j K L m M n N o O P q Q R S T u U V w W x X y Y z Z
+//a A B c C D e E F G H i I j K L m M n o O P q Q R S T U V w W x X y Y z Z
 type Opts struct {
     TempDir     string   `short:"t" long:"tempdir" description:"root dir, relative to CWD, to create mountpoint for the discovered block device" default:"temp"`
     TempPrefix  string   `short:"p" long:"tempname" description:"name of directory to create as a temp mountpoint for discovered block device" default:"camera_sd"`
 
+    DevID       string   `short:"D" long:"dev" description:"device path, relative to /dev/, to scan. Overrides blkidtab,fstypes,fslabels options entirely. Will not override nikonfile flag"`
+    NikonFile   bool     `short:"N" long:"nikon" description:"look for and validate a 512byte file named NIKON001.DSC in the root of the drive"`
+    NikonFilePath string `short:"n" long:"nikonfile" description:"look for and validate a 512byte file with specified name in the root of the drive. Overrides default path assumed by nikon flag"`
+
     BlkidCache  string   `short:"b" long:"blkidtab" description:"path, relative to CWD, for the contents of blkid" default:"/run/blkid/blkid.tab"`
     FsTypes     []string `short:"T" long:"fstypes" description:"filesystem type to examine; call multiple times for multiple types" default:"exfat" default:"fat32"`
-    FsLabels    []string `short:"l" long:"fslabels" description:"filesystem labels to examine, not case-sensitive" default:"nikon"`
+    FsLabels    []string `short:"l" long:"fslabels" description:"filesystem labels to examine, not case-sensitive. Call multiple times for multiple values" default:"nikon"`
 
     RootPath    string   `short:"r" long:"rootdir" description:"root of directory path, relative to CWD, to copy files into." default:"photos"`
     TargetDirs  []string `short:"d" long:"dirname" description:"path, relative to 'rootdir', to create date-formatted directory names. call multiple times to create hardlink clones of the initial directory" default:"."`
     DirFormat   string   `short:"f" long:"dirformat" description:"go-date formatted string for target directory names to move files to" default:"20060102"`
+
+    UserID      int      `short:"u" long:"uid"  description:"numeric user ID to set ownership of files/dirs. setting both user and group IDs will skip checks for rootdir existing" default:-1`
+    GroupID     int      `short:"g" long:"gid"  description:"numeric group ID to set ownership of files/dirs. setting both user and group IDs will skip checks for rootdir existing" default:-1`
 
     MinSize     int      `short:"s" long:"minsize" description:"minimum filesize, in bytes, to be included in the file copy list" default:"1000"`
     Verbose     bool     `short:"v" long:"verbose" description:"print ops while processing"`
