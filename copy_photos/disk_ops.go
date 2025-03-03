d94d74ad43763367a1214436b65cee54205fa481
Daniel De Lellis <dan.delellis@gmail.com>
2025-03-02 20:48:50 -0800

    create a Dev type. move mounting logic to a function of Dev objects. create subdirectories under the created tempdir for each viable block device. add logic to look for the nikon marker file

--- copy_photos/disk_ops.go
+++ copy_photos/disk_ops.go
@@ -5,83 +5,92 @@ import (
     "os"
     "golang.org/x/net/html"
     "github.com/moby/sys/mount"
-    "slices"
     "strings"
 )
 
 const fsTypeKey = "type"
-const mountMode = "ro"
 const fsLabelKey = "label"
+const mountMode = "ro"
+const nikonFile = "NIKON001.DSC"
 
-func getDiskPath(p string, fstypes, labels []string) (diskId, diskFS string, err error) {
-    devs, err := os.Open(p)
-    defer devs.Close()
+func findAndMountDisks(opts *Opts, mountDir string) (targetFS fs.FS, mountedDirs []string, err error) {
+    targetDisks, err := getDiskPaths(opts)
+    if err != nil {
+        err = fmt.Errorf("Unable to locate applicable disk: %v", err)
+        return
+    }
+    if len(targetDisks) == 0 {
+        debug("no valid disks found")
+        return
+    }
+    debug("target disk list", targetDisks)
+
+    for _, v := range targetDisks {
+        err = v.Mount(mountDir, mountMode, opts)
+        if err != nil {
+            return
+        }
+    }
+
+    targetFS = os.DirFS(mountDir)
+    return
+}
+
+func getDiskPaths(opts *Opts) (devIDs []Dev, err error) {
+    if opts.DevIDs == nil {
+        //if no devid is specified, it will scan all devids
+        opts.DevIDs = []string{""}
+    } else {
+        //if devids are specified, assume that whatever label/fstype is discovered is expected
+
+        //label is validated by checking if discovered label has an expected label as a prefix
+        //fstype is validated by checking if expected fs is empty string, or if discovered fs is an exact match of one of the expected filesystems
+        opts.FsTypes = []string{""}
+        opts.FsLabels = []string{""}
+    }
+
+    raw, err := os.Open(opts.BlkidCache)
+    defer raw.Close()
 
     if err != nil {
-        err = fmt.Errorf("Failed opening %s:%s", p, err)
+        err = fmt.Errorf("Failed opening %s:%s", opts.BlkidCache, err)
         return
     }
 
-    data, err := html.Parse(devs)
+    data, err := html.Parse(raw)
     if err != nil {
         err = fmt.Errorf("Failed parsing dev data: %s", err)
         return
     }
 
     for n := range data.Descendants() {
-        ok, fs := validateAttrs(n, fstypes, labels); if ok {
-            diskId = n.Data
-            diskFS = fs
+        ok, fs := validateAttrs(n, opts); if ok {
+            devIDs = append(devIDs, Dev{DevID:n.Data, Filesystem:fs})
         }
     }
-    if diskFS == "" {
-        err = fmt.Errorf("Failed to identify disk filesystem: %s", diskId)
-    }
     return
 }
 
-func findAndMountDisk(cache, mountPoint string, fstypes, labels []string) (targetFS fs.FS, err error) {
-    targetDisk, fsType, err := getDiskPath(cache, fstypes, labels)
-    if err != nil {
-        err = fmt.Errorf("Unable to locate applicable disk: %v", err)
-        return
-    }
-    if targetDisk == "" {
-        return
-    }
-    debug("target disk ", targetDisk,", filesystem ", fsType)
-
-    err = os.Mkdir(mountPoint, 0755)
-    if err != nil {
-        err = fmt.Errorf("Unable to create temp mountpoint: %v", err)
-        return
-    }
-    debug("made mountpoint", mountPoint)
-
-    err = mount.Mount(targetDisk, mountPoint, fsType, mountMode)
-    if err != nil {
-        err = fmt.Errorf("Unable to mount disk to mountpoint: %v", err)
-        return
-    }
-    debug("mounted disk")
-
-    targetFS = os.DirFS(mountPoint)
-    return
-}
-
-func validateAttrs(n *html.Node, fstypes, prefixes []string) (ok bool, fs string) {
+func validateAttrs(n *html.Node, opts *Opts) (ok bool, fs string) {
     if n.Type == html.TextNode && len(n.Parent.Attr) > 0 {
         var hasCorrectLabel, hasCorrectFS bool
 
         debug(fmt.Sprintf("examining attributes for %s", n.Data))
+
+        nameMatch, _ := findEmptyOrMatch(n.Data, opts.DevIDs)
+        if !nameMatch {
+            debugf("%s is not a specified devname", n.Data)
+            return
+        }
+
         for _, v := range n.Parent.Attr {
             debug(fmt.Sprintf("comparing attr %s to %s", v.Key, fsLabelKey))
             if v.Key == fsLabelKey {
-                hasCorrectLabel = findLabelWithPrefix(prefixes, v.Val)
+                hasCorrectLabel = findLabelWithPrefix(opts.FsLabels, v.Val)
             }
 
             if v.Key == fsTypeKey {
-                hasCorrectFS, fs = findFsType(fstypes, v.Val)
+                hasCorrectFS, fs = findFsType(v.Val, opts.FsTypes)
             }
             debug("found fs with expected label:", hasCorrectLabel, "; found fs with expected type:", hasCorrectFS)
             if hasCorrectLabel && hasCorrectFS && fs != "" {
@@ -94,11 +103,15 @@ func validateAttrs(n *html.Node, fstypes, prefixes []string) (ok bool, fs string
     return
 }
 
-func findFsType(fstypes []string, fs string) (bool, string) {
-    if slices.Contains(fstypes, fs) {
-        return true, fs
+func findFsType(fs string, fstypes []string) (bool, string) {
+    return findEmptyOrMatch(fs, fstypes)
+}
+func findEmptyOrMatch(discovered string, expected []string) (bool, string) {
+    for _, exp := range expected {
+        if exp == "" || exp == discovered {
+            return true, discovered
+        }
     }
-
     return false, ""
 }
 
@@ -110,4 +123,43 @@ func findLabelWithPrefix(prefixes []string, label string) bool {
         }
     }
     return false
+}
+
+func (dev *Dev) Mount(location, mode string, opts *Opts) ( err error ) {
+    mountPoint := location + "/" + dev.DevID
+    err = os.MkdirAll(mountPoint, 0755)
+    if err != nil {
+        err = fmt.Errorf("Unable to create temp dir for mountpoint: %v", err)
+        return
+    }
+    debug("made mountpoint", mountPoint)
+
+    err = mount.Mount(dev.DevID, mountPoint, dev.Filesystem, mode)
+    if err != nil {
+        err = fmt.Errorf("Unable to mount disk to mountpoint: %v", err)
+        return
+    }
+    debug("mounted disk")
+    if opts.NikonFile {
+        if opts.NikonFilePath == "" {
+            opts.NikonFilePath = nikonFile
+        }
+    }
+    if opts.NikonFilePath != "" {
+        r, e := readData(mountPoint+"/"+nikonFile,512)
+        if e != nil {
+            err = fmt.Errorf("Called with --nikon or --nikonfile but %s not found in root of %s", nikonFile, dev.DevID)
+            return
+        }
+
+        if !compareByteSlices(r, make([]byte,512)) {
+            err = fmt.Errorf("called with --nikon or --nikonfile but %s at root of %s is not a 512-byte file of 0s", nikonFile, dev.DevID)
+            return
+        }
+    }
+    return
+}
+
+type Dev struct {
+    DevID, Filesystem string
 }
