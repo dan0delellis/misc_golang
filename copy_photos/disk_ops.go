e8d63ba0dfeed7e59401cd8cfbf6f5ccc34570e8
Daniel De Lellis <dan.delellis@gmail.com>
2025-03-03 02:19:23 -0800

    move the dev struct stuff to its own file

--- copy_photos/disk_ops.go
+++ copy_photos/disk_ops.go
@@ -4,7 +4,6 @@ import (
     "io/fs"
     "os"
     "golang.org/x/net/html"
-    "github.com/moby/sys/mount"
     "strings"
 )
 
@@ -126,42 +125,4 @@ func findLabelWithPrefix(prefixes []string, label string) bool {
         }
     }
     return false
-}
-
-func (dev *Dev) Mount(mountPoint, mode string, opts *Opts) ( err error ) {
-    err = os.MkdirAll(mountPoint, 0755)
-    if err != nil {
-        err = fmt.Errorf("Unable to create temp dir for mountpoint: %v", err)
-        return
-    }
-    debug("made mountpoint", mountPoint)
-
-    err = mount.Mount(dev.DevID, mountPoint, dev.Filesystem, mode)
-    if err != nil {
-        err = fmt.Errorf("Unable to mount disk to mountpoint: %v", err)
-        return
-    }
-    debug("mounted disk")
-    if opts.NikonFile {
-        if opts.NikonFilePath == "" {
-            opts.NikonFilePath = nikonFile
-        }
-    }
-    if opts.NikonFilePath != "" {
-        r, e := readData(mountPoint+"/"+nikonFile,512)
-        if e != nil {
-            err = fmt.Errorf("Called with --nikon or --nikonfile but %s not found in root of %s", nikonFile, dev.DevID)
-            return
-        }
-
-        if !compareByteSlices(r, make([]byte,512)) {
-            err = fmt.Errorf("called with --nikon or --nikonfile but %s at root of %s is not a 512-byte file of 0s", nikonFile, dev.DevID)
-            return
-        }
-    }
-    return
-}
-
-type Dev struct {
-    DevID, Filesystem string
 }
