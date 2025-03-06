5db9f01087e6d8b48d9a9c3127bb05ab5e099115
Daniel De Lellis <dan.delellis@gmail.com>
2025-03-05 22:51:45 -0800

    define constants where they are used

--- lcd_readout/backlight.go
+++ lcd_readout/backlight.go
@@ -4,6 +4,30 @@ import (
     "golang.org/x/image/colornames"
 )
 
+var BrightnessNames = []string{
+    "off",
+    "faint",
+    "dim",
+    "soft",
+    "moderate",
+    "bright",
+    "vivid",
+    "brilliant",
+    "max",
+}
+
+var BrightnessMap = map[string]uint8{
+    BrightnessNames[0]: 0,
+    BrightnessNames[1]: 16,
+    BrightnessNames[2]: 32,
+    BrightnessNames[3]: 64,
+    BrightnessNames[4]: 96,
+    BrightnessNames[5]: 128,
+    BrightnessNames[6]: 160,
+    BrightnessNames[7]: 192,
+    BrightnessNames[8]: 255,
+}
+
 // SVGColorKeyword sets the background color to the string provided, if said string has a corresponding value in the image/colornames library
 // If the provided string is not a known color name, BG will not be changed
 func (d Display) ColorKeyword(name string) (err error) {
